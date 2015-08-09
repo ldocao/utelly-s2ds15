245f0b9b9054a33a36a469fc0e8c541f4dd11b0c
ldocao <ldocao@gmail.com>
2015-08-09 20:02:48 +0100

    properly define collection, event and cluster classes

--- clustering/common.py
+++ clustering/common.py
@@ -1,27 +1,12 @@
 import uuid
-from utils import becomes_list, add_to_list
-import metric.numerical_distance as numdist
+from utils import becomes_list
+import metrics.numerical_distance as numdist
 
 
 
 ##duration should be expressed as datetime.timedelta object
 UNKNOWN = None #define what should be put if unknown or not available
 
-
-class Group():
-    """Group found from clustering"""
-
-    def __init__(self, keywords, members):
-        self._id = uuid.uuid4() ##need to change uuid to pymongo objectid
-        self.keywords = becomes_list(keywords)
-        self.members = becomes_list(members)
-
-    def add_member(self, new_member):
-        self.members = add_to_list(self.members,new_member)
-
-
-    def __repr__(self):
-        return str(self.__class__.__name__)+"("+str(len(self.members))+")"
 
 
 
@@ -36,17 +21,80 @@ class Event():
     """
 
     def __init__(self, **kwargs):
-        self._id = uuid.uuid4()
-
-        ##recursively define class attributes
+        self._event_id = uuid.uuid4()
         for key, value in kwargs.items():
             setattr(self, key, value)
+            
+        #self.attributes = dict(kwargs) #dynamic variable names
+
+    def __repr__(self):
+        return str(self.__class__.__name__)+"("+str(self._event_id)+")"
+
+
+
+            
+
+class Cluster():
+    """Group found from clustering"""
+
+    def __init__(self, members=[], keywords=[]):
+        self._cluster_id = uuid.uuid4() ##need to change uuid to pymongo objectid
+        self.members = becomes_list(members)
+        self.keywords = becomes_list(keywords)
+
+    def add(self,new):
+        try:
+            self.members.extend(new)
+        except TypeError:
+            self.members.append(new)
+
+
+    def __repr__(self):
+        return str(self.__class__.__name__)+"("+str(len(self.members))+")"
+
+
+    def __getattr__(self, name_of_attribute):
+        return [m.__dict__.get(name_of_attribute, UNKNOWN) for m in self.members]
 
 
 
 
 
+    
+class Collection(list):
+    """Collection of objects, mongo db alike
+
+    Parameters:
+    name: string
+        name of collection
+
+    objects: list of objects
+        All events belonging to the collection. Must have the same structure.
+    """
+
+
+    ##need to define a collection efficiently. see this ? http://stackoverflow.com/questions/683/using-in-to-match-an-attribute-of-python-objects-in-an-array
+    
+    def __init__(self, members=[]):
+        self._collection_id = uuid.uuid4()
+        self.members = becomes_list(members)
         
+    def add(self,new):
+        try:
+            self.members.extend(new)
+        except TypeError:
+            self.members.append(new)
+
+    def __repr__(self):
+        return self.members
+
+    def __getattr__(self, name_of_attribute):
+        return [m.__dict__.get(name_of_attribute, UNKNOWN) for m in self.members]
+
+
+
+
+           
 class Actor():
     """Actor(ress)"""
 
@@ -68,6 +116,9 @@ class Metric():
 
     def __repr__(self):
         return self.name
+
+
+
 
 
 
